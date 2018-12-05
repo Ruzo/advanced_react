33d109e24bfa8d05f91f9f9d85ce4f02e627dfd0
Richard Rouzeau <richard@evigy.com>
2018-12-05 09:21:24 -0500

    Chapter 1

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class TheApp extends Component {
+  render () {
+    return (
+      <div>
+        <p>
+          Helloooo nurse!
+        </p>
+      </div>
+    )
+  }
+}
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Sell extends Component {
+  render () {
+    return (
+      <div>
+        <p>
+          Sell!
+        </p>
+      </div>
+    )
+  }
+}
